e0d2e1b8075d404fc5163827180041fa0e39b4a0
Kendrick Gruenberg <kendrick.gruenberg@t-online.de>
2024-01-29 19:35:36 +0100

    add Package.swift, cf. https://developer.apple.com/documentation/xcode/distributing-binary-frameworks-as-swift-packages

--- Package.swift
+++ Package.swift
@@ -14,8 +14,10 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "SimplyMail"),
+        .binaryTarget(
+                    name: "SimplyMail",
+                    path: "rust_lib_framework.xcframework"
+                ),
         .testTarget(
             name: "SimplyMailTests",
             dependencies: ["SimplyMail"]),
