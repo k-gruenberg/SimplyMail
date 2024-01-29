ccc50e5da32e1cc6370d43a8ec59cc97c60af609
Kendrick Gruenberg <kendrick.gruenberg@t-online.de>
2024-01-29 19:37:13 +0100

    added modified build.sh from Filip W's repository

---
+++ build.sh
@@ -0,0 +1,28 @@
+cd rust-lib
+
+HEADERPATH="bindings/rust_libFFI.h"
+TARGETDIR="target"
+OUTDIR=".."
+RELDIR="release"
+NAME="rust_lib"
+STATIC_LIB_NAME="lib${NAME}.a"
+NEW_HEADER_DIR="bindings/include"
+
+cargo build --target aarch64-apple-darwin --release
+cargo build --target aarch64-apple-ios --release
+cargo build --target aarch64-apple-ios-sim --release
+
+mkdir -p "${NEW_HEADER_DIR}"
+cp "${HEADERPATH}" "${NEW_HEADER_DIR}/"
+cp "bindings/rust_libFFI.modulemap" "${NEW_HEADER_DIR}/module.modulemap"
+
+rm -rf "${OUTDIR}/${NAME}_framework.xcframework"
+
+xcodebuild -create-xcframework \
+    -library "${TARGETDIR}/aarch64-apple-darwin/${RELDIR}/${STATIC_LIB_NAME}" \
+    -headers "${NEW_HEADER_DIR}" \
+    -library "${TARGETDIR}/aarch64-apple-ios/${RELDIR}/${STATIC_LIB_NAME}" \
+    -headers "${NEW_HEADER_DIR}" \
+    -library "${TARGETDIR}/aarch64-apple-ios-sim/${RELDIR}/${STATIC_LIB_NAME}" \
+    -headers "${NEW_HEADER_DIR}" \
+    -output "${OUTDIR}/${NAME}_framework.xcframework"
